9ec6d016f6d8a8338bdbef3469142443f91c670f
Charles Lambrecht <lambrechtca@gmail.com>
2017-04-11 14:26:40 -0400

    Statement for even numbers

--- Lab2/Program.cs
+++ Lab2/Program.cs
@@ -16,12 +16,31 @@ namespace Lab2
             string name;
             name = Console.ReadLine();
 
+            Console.Clear();
             Console.WriteLine("Hello " + name + "! \nPlease enter an integer between 1 and 100.");
 
             int input;
             input = int.Parse(Console.ReadLine());
 
-            if ( input % 2 == 0)
+            string output;
+
+            if ( input % 2 == 0) //if statement for even numbers
+            {
+                if (input <= 25)
+                {
+                    Console.WriteLine("Thanks " + name + "! \nThat number is even and less than 25");
+                }
+                else if (input > 60)
+                {
+                    Console.WriteLine("Thanks " + name + "! \n" + input + " is even.");
+                }
+            }
+            
+            else if ( input % 2 == 1) //else statement for odd numbers
+            {
+
+            }
+            else //error?
             {
 
             }
