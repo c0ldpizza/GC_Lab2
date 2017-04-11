400518212385000d38a7ce0cedf5012ca8f21249
Charles Lambrecht <lambrechtca@gmail.com>
2017-04-11 14:11:47 -0400

    Initial commit. Gather user input

---
+++ Lab2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //input
+            Console.WriteLine("What is your name?");
+
+            string name;
+            name = Console.ReadLine();
+
+            Console.WriteLine("Hello " + name + "! \nPlease enter an integer between 1 and 100.");
+
+            int input;
+            input = int.Parse(Console.ReadLine());
+
+            if ( input % 2 == 0)
+            {
+
+            }
+        }
+    }
+}
